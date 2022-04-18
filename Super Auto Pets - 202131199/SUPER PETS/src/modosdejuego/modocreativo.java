2b67aa845dc2a7778b8bb7a6388616746a559ec1
AndresFernandoGonzalezAlcantara <99780343+AndresFernandoGonzalezAlcantara@users.noreply.github.com>
2022-04-18 07:56:44 -0600

    Delete Super Auto Pets - 202131199 directory

--- Super Auto Pets - 202131199/SUPER PETS/src/modosdejuego/modocreativo.java
+++
@@ -1,8 +0,0 @@
-package modosdejuego;
-
-public class modocreativo {
-    public void inicio()
-    {
-        System.out.println("---------MODO CREATIVO---------");
-    }
-}
--- Super Auto Pets - 202131199/SUPER PETS/src/modosdejuego/modoversus.java
+++
@@ -1,9 +0,0 @@
-package modosdejuego;
-
-public class modoversus {
-    public void inicio()
-    {
-        System.out.println("---------MODO VERSUS---------");
-
-    }
-}
